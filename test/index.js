83b6791fab452b5564acc2c68721151a47b07fea
Kevin Locke <kevin@kevinlocke.name>
2022-07-02 20:50:07 -0600

    style: style fixes for new ESLint rules
    Use top-level await where appropriate.
    Use node: for Node.js imports.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
  * @license MIT
  */
 
-import assert from 'assert';
+import assert from 'node:assert';
 
 import OpenApiTransformerPipeline from '../index.js';
 import AsyncTransformer from '../test-lib/async-transformer.js';
